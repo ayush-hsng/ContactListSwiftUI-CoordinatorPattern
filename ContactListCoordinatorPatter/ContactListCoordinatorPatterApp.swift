28baac570d4ba50cd7b3294def96406a80db2c0e
ayush-hsng <ayush.sinha@housing.com>
2024-01-23 10:01:45 +0530

    Initial Commit

---
+++ ContactListCoordinatorPatter/ContactListCoordinatorPatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListCoordinatorPatterApp.swift
+//  ContactListCoordinatorPatter
+//
+//  Created by Ayush Kumar Sinha on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactListCoordinatorPatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactListCoordinatorPatter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactListCoordinatorPatter
+//
+//  Created by Ayush Kumar Sinha on 23/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
